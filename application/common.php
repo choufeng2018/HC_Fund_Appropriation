4d934c66a4900f774d3df7635167bfa3014659a3
xpwsgg <xpwsgg@163.com>
2018-11-09 11:57:58 +0800

    新增一个数字转中文的方法; 拨款记录页面将数字的拨款记录改成汉字显示;

--- application/common.php
+++ application/common.php
@@ -98,3 +98,51 @@ if (!function_exists('get_adminName_by_id')) {
         return $admin_name;
     }
 }
+
+if (!function_exists('num2Word')) {
+    /**
+     * @param $num
+     * @return mixed|string
+     * 数字转中文
+     */
+    function num2Word($num)
+    {
+        $chiNum = array('零', '一', '二', '三', '四', '五', '六', '七', '八', '九');
+        $chiUni = array('','十', '百', '千', '万', '亿', '十', '百', '千');
+
+        $num_str = (string)$num;
+
+        $count = strlen($num_str);
+        $last_flag = true; //上一个 是否为0
+        $zero_flag = true; //是否第一个
+        $temp_num = null; //临时数字
+
+        $chiStr = '';//拼接结果
+        if ($count == 2) {//两位数
+            $temp_num = $num_str[0];
+            $chiStr = $temp_num == 1 ? $chiUni[1] : $chiNum[$temp_num].$chiUni[1];
+            $temp_num = $num_str[1];
+            $chiStr .= $temp_num == 0 ? '' : $chiNum[$temp_num];
+        }else if($count > 2){
+            $index = 0;
+            for ($i=$count-1; $i >= 0 ; $i--) {
+                $temp_num = $num_str[$i];
+                if ($temp_num == 0) {
+                    if (!$zero_flag && !$last_flag ) {
+                        $chiStr = $chiNum[$temp_num]. $chiStr;
+                        $last_flag = true;
+                    }
+                }else{
+                    $chiStr = $chiNum[$temp_num].$chiUni[$index%9] .$chiStr;
+
+                    $zero_flag = false;
+                    $last_flag = false;
+                }
+                $index ++;
+            }
+        }else{
+            $chiStr = $chiNum[$num_str[0]];
+        }
+        return $chiStr;
+    }
+}
